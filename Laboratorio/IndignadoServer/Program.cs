a66fa7c11a556fabb45469c081fd0583b05cd6d8
Agustin <agustin@lavabit.com>
2012-04-26 13:22:07 -0300

    Primer commit para vs 2010

---
+++ Laboratorio/IndignadoServer/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.ServiceModel;
+
+namespace IndignadoServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost _svcHost = null;
+
+            try
+            {
+                Console.WriteLine("Starting Expense Host ...");
+
+                _svcHost = LoadTestService();
+
+                Console.WriteLine("\n Indignado Server started. Press any key to exit.\n\n");
+                Console.Read();
+            }
+            catch
+            {
+                Console.WriteLine("\nHost initialization has failed.");
+                Console.WriteLine("Press any key to terminate.");
+                Console.Read();
+            }
+            finally
+            {
+                if (_svcHost != null)
+                    _svcHost.Close();
+            }
+        }
+
+        private static ServiceHost LoadTestService()
+        {
+            ServiceHost svcHost = null;
+            try
+            {
+                Console.Write("Loading Test Service ... ");
+
+                // Create Service Host.
+                svcHost = new ServiceHost(typeof(TestService));
+                svcHost.Open();
+
+                Console.WriteLine("Done!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nError Loading Test Service. ");
+                Console.WriteLine("Exception : " + ex.Message);
+                throw ex;
+            }
+
+            return svcHost;
+        }
+
+    }
+}
---
+++ Laboratorio/IndignadoWeb/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ServiceModel;
+
+namespace IndignadoWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ChannelFactory<ServiceReference1.ITestService> scf;
+            scf = new ChannelFactory<ServiceReference1.ITestService>(
+                        new BasicHttpBinding(),
+                        "http://localhost:8732/IndignadoServer/TestService/");
+
+
+            ServiceReference1.ITestService s;
+            s = scf.CreateChannel();
+
+            ViewBag.Message = s.Ping("Pancho");
+
+            (s as ICommunicationObject).Close();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
