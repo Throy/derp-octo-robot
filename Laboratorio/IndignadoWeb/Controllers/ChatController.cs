47b9618fc45655b9496ec4e28b8f2ddee64e78da
Agustin <agustin@lavabit.com>
2012-05-30 19:21:48 -0300

    chat web

---
+++ Laboratorio/IndignadoWeb/Controllers/ChatController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IndignadoWeb.Common;
+using IndignadoWeb.Models;
+using IndignadoWeb.SessionServiceReference;
+
+namespace IndignadoWeb.Controllers
+{
+    [MultiTenanActionFilter]
+    public class ChatController : Controller
+    {
+        //
+        // GET: /Chat/
+
+        public JsonResult StartSession()
+        {
+            ChatBox chatBox = new ChatBox();
+
+            DTLoginInfo loginInfo = HttpContext.Session["loginInfo"] as DTLoginInfo;
+            if (loginInfo != null)
+                chatBox.username = loginInfo.name;
+            else
+                chatBox.username = "DonNadie";
+
+            Message m = new Message();
+            m.f = "Pancho";
+            m.s = "1";
+            m.m = "Test chat hardcoded";
+
+            chatBox.items = new List<Message>();
+            chatBox.items.Add(m);
+
+            return Json(chatBox, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Heartbeat()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Close()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Send()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Laboratorio/IndignadoWeb/Models/ChatModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IndignadoWeb.Models
+{
+    public class Message
+    {
+        public String s { get; set; }
+        public String f { get; set; }
+        public String m { get; set; }
+    }
+
+    public class ChatBox
+    {
+        public String username { get; set; }
+        public List<Message> items { get; set; }
+    }
+}
